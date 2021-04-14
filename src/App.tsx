3e32ddd5cd27331e5e20e5dad6280af9cacc6ab8
Edward Reyes <edwardreyessp@gmail.com>
2021-04-14 15:00:00 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
+const Counter = ({count} : {count : number}) => (
+  <h1>
+    Salto: {count}
+  </h1>
+)
+
+const Counter2 = ({count} : {count : number}) => (
+  <h1>
+    La cuenta actual es: {count}
+  </h1>
+)
+
+const App = () => {
+  const [count, setCount] = useState(0)
+  const [count2, setCount2] = useState(1)
+
+  const add = () => setCount(count + count2)
+  const substract = () => setCount(count - count2)
+
+  const addSalto = () => setCount2(count2 + 1)
+  const subSalto = () => setCount2(count2 - 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main className="App">
+      <h1 className="title">Contador con salto</h1>
+      <div className="counter">
+        <section>
+          <Counter count={count2}/>
+          <div className="buttons">
+            <button onClick={addSalto}>Aumentar</button>
+            <button onClick={subSalto}>Disminuir</button>
+            <button onClick={() => setCount2(1)}>Reset</button>
+          </div>
+        </section>
+        <section>
+          <Counter2 count={count}/>
+          <div className="buttons">
+            <button onClick={add}>Aumentar</button>
+            <button onClick={substract}>Disminuir</button>
+            <button onClick={() => setCount(0)}>Reset</button>
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
 
-export default App;
+export default App
